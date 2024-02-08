c763615acafdf14fbb5dd51f2e94307e21892945
ZGako <lhgino2003@gmail.com>
2024-02-08 17:53:32 +0100

    finito /auth testing

--- __test__/apiAuth.test.ts
+++ __test__/apiAuth.test.ts
@@ -1,29 +1,31 @@
 /**
  * @jest-environment node
  */
-import { createMocks, RequestMethod } from 'node-mocks-http';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import jwt from "jsonwebtoken";
-import gatewaysHandler from './gatewaysHandler';
+import { gatewaysHandler, mockRequestResponse } from './testingSupportFunctions';
+import mongoose from 'mongoose';
+import User from "@/app/models/userModel";
+import { connectDB } from '@/configs/dbConfig';
+
+
 axios.defaults.baseURL = "http://localhost:3000";
+
 
 describe('first test', () => {
 
-    function mockRequestResponse(method: RequestMethod = 'GET') {
-        const {
-            req,
-            res,
-        }: {
-            req: NextApiRequest, res: NextApiResponse
-        } = createMocks({ method });
+    beforeAll(async () => {
+        connectDB();
+    });
+    
+    afterAll(async () => {
 
-        req.headers = {
-            'content-type': 'application/json',
-          };
+    // delete all testing users (in this case used for mock registration)
+    await User.deleteOne({email: "test1@gmail.com"}).exec();
 
-        return {req, res};
-    }
+    await mongoose.connection.close();
+
+    });
 
     const userData = {
         _id:"65b459485cb533882222ef98",
@@ -35,6 +37,13 @@ describe('first test', () => {
         isVerified:true,
         password: "mail",
         isActive:true
+    }
+
+    const librarianCredentials = {
+        _id:"65c0dc780b55f3bdcbf9f234",
+        username:"admin1",
+        password:"HhM15wjBql9N!k!U",
+        "isAdmin":true,
     }
 
     it("'/auth/login/user-login' with correct credentials", async () => {
@@ -137,6 +146,142 @@ describe('first test', () => {
         await gatewaysHandler(req, res);
         expect(res.statusCode).toBe(400);
         expect((res as any)._getJSONData()).toEqual({message: 'jwt expired'});
+    });
+
+    it("'/auth/logout' test", async () => {
+
+        const {req, res} = mockRequestResponse("DELETE");
+        
+        req.url="/api/auth/logout";
+        
+        await gatewaysHandler(req, res);
+        expect(res.statusCode).toBe(200);
+        expect((res as any)._getJSONData()).toEqual({message: 'Logout successful'});
+    });
+
+    it("'/auth/login/librarian-login' with correct credentials", async () => {
+
+        const {req, res} = mockRequestResponse("POST");
+
+        req.url="/api/auth/login/librarian-login";
+
+        req.body = {
+            username: librarianCredentials.username,
+            password: librarianCredentials.password
+        };
+        
+        await gatewaysHandler(req, res);
+        expect(res.statusCode).toBe(200);
+        expect((res as any)._getJSONData()).toEqual({ message: 'Login successful' });
+    });
+
+    it("'/auth/login/librarian-login' with incorrect password", async () => {
+
+        const {req, res} = mockRequestResponse("POST");
+
+        req.url="/api/auth/login/librarian-login";
+
+        req.body = {
+            username: librarianCredentials.username,
+            password: "incorrect password"
+        };
+        
+        await gatewaysHandler(req, res);
+        expect(res.statusCode).toBe(400);
+        expect((res as any)._getJSONData()).toEqual({ message: 'Invalid credentials' });
+    });
+
+    it("'/auth/login/librarian-login' with non existant user", async () => {
+
+        const {req, res} = mockRequestResponse("POST");
+
+        req.url="/api/auth/login/librarian-login";
+
+        req.body = {
+            username: "non existant user",
+            password: "incorrect password"
+        };
+        
+        await gatewaysHandler(req, res);
+        expect(res.statusCode).toBe(400);
+        expect((res as any)._getJSONData()).toEqual({ message: 'User does not exist' });
+    });
+
+    it("'/auth/register' with new user", async () => {
+
+        const {req, res} = mockRequestResponse("POST");
+
+        req.url="/api/auth/register";
+
+        req.body = {
+            name:"Marco",
+            surname:"Rossi",
+            codiceFiscale:"fdffsddf466fdfaa",
+            dateOfBirth:"2004-03-24T00:00:00.000Z",
+            email:"test1@gmail.com",
+            password: "mail",
+        };
+        
+        await gatewaysHandler(req, res);
+        expect(res.statusCode).toBe(200);
+        expect((res as any)._getJSONData()).toEqual({ message: 'User created successfully' });
+    });
+
+    it("'/auth/register' with duplicate key error", async () => {
+
+        const {req, res} = mockRequestResponse("POST");
+
+        req.url="/api/auth/register";
+
+        req.body = {
+            name:"Marco",
+            surname:"Rossi",
+            codiceFiscale:"fdffsddf466fdfsd",
+            dateOfBirth:"2004-03-24T00:00:00.000Z",
+            email:"test3@gmail.com",
+            password: "mail",
+        };
+        
+        await gatewaysHandler(req, res);
+        expect(res.statusCode).toBe(400);
+        expect((res as any)._getJSONData()).toEqual({ message: 'E11000 duplicate key error collection: test.users index: codiceFiscale_1 dup key: { codiceFiscale: \"fdffsddf466fdfsd\" }' });
+    });
+
+    it("'/auth/register' with incomplete information", async () => {
+
+        const {req, res} = mockRequestResponse("POST");
+
+        req.url="/api/auth/register";
+
+        req.body = {
+            email:"test2@gmail.com",
+            password: "mail",
+        };
+        
+        await gatewaysHandler(req, res);
+        expect(res.statusCode).toBe(400);
+        expect((res as any)._getJSONData()).toEqual(
+            { message: 'users validation failed: dateOfBirth: Path `dateOfBirth` is required., codiceFiscale: Path `codiceFiscale` is required., surname: Path `surname` is required., name: Path `name` is required.' });
+    });
+    
+
+    it("'/auth/register' with existing user", async () => {
+
+        const {req, res} = mockRequestResponse("POST");
+
+        req.url="/api/auth/register";
+
+        req.body = {
+            name:"Giacomo",
+            surname:"Lanzo",
+            codiceFiscale:"fdffsddf466fdfsd",
+            email:"mail@gmail.com",
+            password: "mail",
+        };
+        
+        await gatewaysHandler(req, res);
+        expect(res.statusCode).toBe(400);
+        expect((res as any)._getJSONData()).toEqual({ message: 'User already exists' });
     });
 
 })
--- __test__/testingSupportFunctions.ts
+++ __test__/testingSupportFunctions.ts
@@ -1,7 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { RequestMethod, createMocks } from 'node-mocks-http';
 
-export default async function gatewaysHandler(
+export async function gatewaysHandler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
@@ -20,6 +21,22 @@ export default async function gatewaysHandler(
     res.status(200).json(response.data);
   } catch (err: any) {
     // Check if we got a useful response
+    // console.log(err.response.data);
     res.status(400).json(err.response.data);
   }
+}
+
+export function mockRequestResponse(method: RequestMethod = 'GET') {
+  const {
+      req,
+      res,
+  }: {
+      req: NextApiRequest, res: NextApiResponse
+  } = createMocks({ method });
+
+  req.headers = {
+      'content-type': 'application/json',
+    };
+
+  return {req, res};
 }
